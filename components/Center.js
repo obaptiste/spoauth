d4995f2d3b24fbf2347ed989df7130d2dfabdb71
Oris John-Baptiste <orisjb@MacBook-Air.local>
2022-09-30 01:06:27 +0100

    added some styling

---
+++ components/Center.js
@@ -0,0 +1,42 @@
+import { ChevronDownIcon } from '@heroicons/react/outline'
+import { useSession } from 'next-auth/react';
+import React, {useEffect, useState} from 'react'
+import {shuffle} from "lodash"
+
+const colors = [
+    "from-indigo-500",
+    "from-blue-500",
+    "from-purple-500",
+    "from-red-500",
+    "from-yellow-500",
+    "from-green-500",
+    "from-pink-500",
+]
+
+function Center() {
+    const {data: session} = useSession();
+    const [color, setColor] = useState(null);
+
+    useEffect(() => {
+        if(session){
+            setColor(shuffle(colors).pop());
+        }
+        }, []);  // shuf
+
+  return (
+    <div className='flex-grow text-white'>
+        <header>
+            <div className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2'>
+                <img className="rounded-full w-10 h-10" src={session?.user.image} alt=""/>
+            </div>
+            <ChevronDownIcon className="h-5 w-5" />
+        </header>
+        <section className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white padding-8`}>
+            <h1>hello</h1>
+        </section>
+
+    </div>
+  )
+}
+
+export default Center
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,15 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Sidebar from '../components/sidebar';
+import Center from '../components/center';
 
 
 const Home: NextPage = () => {
   return (
    <div className='bg-black h-screen overflow-hidden'>
-    <main>
+    <main className='flex'>
     <Sidebar/>
-    {/* center */}
+    <Center/>
     </main>
 
     {/* player */}
